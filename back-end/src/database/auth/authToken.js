0fe96f53cd7667e0d15326c348aafaa3e9b23e7c
Lilibertola <bertolaccini55@gmail.com>
2023-05-05 13:42:36 -0300

    teste-login -não esta terminado

--- back-end/src/database/auth/authToken.js
+++ back-end/src/database/auth/authToken.js
@@ -1,11 +1,10 @@
 import * as jwt from 'jsonwebtoken';
-import { ILogin } from '../intefaces/ILogin';
 
 const secret = process.env.JWT_SECRET || 'secret';
 
-export const generateToken = (payload: ILogin) => jwt.sign({ payload }, secret, {
+export const generateToken = (payload) => jwt.sign({ payload }, secret, {
   algorithm: 'HS256',
   expiresIn: '7d',
 });
 
-export const verifyToken = (token: string) => jwt.verify(token, secret);
+export const verifyToken = (token) => jwt.verify(token, secret);
