61566bd46ec700702e75a12099aa35fddff14346
Yanni <yannidilly.prog@gmail.com>
2023-05-05 15:00:16 -0300

    fix: sales import

--- back-end/src/tests/integration/Sale.test.js
+++ back-end/src/tests/integration/Sale.test.js
@@ -4,7 +4,7 @@ const { expect } = require('chai');
 const chaiHttp = require('chai-http');
 
 const app = require('../../api/app');
-// const Sale = require('../../database/models');
+const { Sale } = require('../../database/models');
 const salesMock = require('../mocks/Sale');
 
 chai.use(chaiHttp);
@@ -17,7 +17,7 @@ describe('Verifica se é possível ver os detalhes de um pedido', function () {
   it(
   'Testa se é retornado os detalhes de um pedido de acordo com o id passado na url',
   async function () {
-    // sinon.stub(Sale, 'findAll').resolves(salesMock.salesDetails); -> está dando erro no mock do findAll (diz que não existe)
+    sinon.stub(Sale, 'findAll').resolves(salesMock.salesDetails);
 
     const response = await chai.request(app).get('/seller/orders/details/1');
     expect(response.status).to.be.equals(200);
