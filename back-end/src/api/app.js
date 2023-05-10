e41e3496a27ba55c107519370159b65548d3f6a2
Lucas-De-Lima <lucasramosdelima@gmail.com>
2023-05-10 18:18:10 -0300

    feat: create OrderCard component for render card in SellerOrders component

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -3,6 +3,7 @@ const cors = require('cors');
 const { userRoutes, productRoutes, saleRoutes, saleProductRouter } = require('../database/routes');
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 app.use(cors());
 app.use(express.static('public'));
