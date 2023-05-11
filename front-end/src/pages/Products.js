5cc04ca32286f19fedb48e64626368f3144614e2
mirelladourado <mirellaalvesdourado@gmail.com>
2023-05-11 13:09:45 -0300

    feat: função subCart + botão carrinho

--- front-end/src/pages/Products.js
+++ front-end/src/pages/Products.js
@@ -19,6 +19,14 @@ class Products extends React.Component {
     this.getShoppingCart();
   };
 
+  componentDidUpdate(_prevProps, prevState) {
+    const {shoppingCartValue, shoppingCart} = this.state;
+    if (prevState.shoppingCartValue !== shoppingCartValue) {
+      localStorage.setItem('shoppingCart', JSON.stringify(shoppingCart));
+      localStorage.setItem('shoppingCartValue', JSON.stringify(shoppingCartValue))
+    }
+  }
+
   getProducts = async () => {
     const products = await requestGet('product');
     this.setState({
@@ -28,11 +36,27 @@ class Products extends React.Component {
 
   getShoppingCart = () => {
     const shoppingCart = localStorage.getItem('shoppingCart');
+    const shoppingCartValue = localStorage.getItem('shoppingCartValue');
     if (shoppingCart === null) {
-      localStorage.setItem('shoppingCart', JSON.stringify({}))
+      localStorage.setItem('shoppingCart', JSON.stringify({}));
+      localStorage.setItem('shoppingCartValue', JSON.stringify(0))
     } else {
       this.setState({
-        shoppingCart: JSON.parse(shoppingCart)
+        shoppingCart: JSON.parse(shoppingCart),
+        shoppingCartValue: JSON.parse(shoppingCartValue)
+      })
+    }
+  };
+
+  subCart = (item, itemPrice) => {
+    const { shoppingCart, shoppingCartValue } = this.state;
+    if (shoppingCart[item]) {
+      this.setState({
+        shoppingCart: {
+          ...shoppingCart,
+          [item]: shoppingCart[item] - 1
+        },
+        shoppingCartValue: shoppingCartValue - Number(itemPrice)
       })
     }
   };
@@ -74,6 +98,7 @@ class Products extends React.Component {
             <div>
               <button
                 data-testid={`customer_products__button-card-rm-item-${index}`}
+                onClick={ () => this.subCart(product.name, product.price) }
               >
                 -
               </button>
@@ -91,6 +116,13 @@ class Products extends React.Component {
           </div>
         )
       })}
+      <button>
+        Ver Carrinho: R$
+        {' '}
+        <span data-testid='customer_products__checkout-bottom-value'>
+          {(this.state.shoppingCartValue).toFixed(2).replace(".", ",")}
+        </span>
+      </button>
       </>
     );
   }
