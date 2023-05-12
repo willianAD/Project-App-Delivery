0a4c6079fc16453367c9d2df511147f72d8243ea
Yanni <yannidilly.prog@gmail.com>
2023-05-12 15:59:39 -0300

    fix: change saleProductsDetails params (id -> token)

--- back-end/src/database/routes/SaleRoutes.js
+++ back-end/src/database/routes/SaleRoutes.js
@@ -9,7 +9,7 @@ saleRoutes.get('/orders', sale.getAll);
 
 saleRoutes.post('/orders', sale.create);
 
-saleRoutes.get('/:id', sale.getDetailsById);
+saleRoutes.get('/details', sale.getDetailsById);
 
 saleRoutes.put('/:id', sale.putDetails);
 
