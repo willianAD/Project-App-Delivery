0614a6f9458212eaf3def1350a2e582b53208e59
Lucas-De-Lima <lucasramosdelima@gmail.com>
2023-05-12 15:51:29 -0300

    feat: creat html elements and data-testid for component "Navbar.js"

--- front-end/src/components/SellerOrders/Navbar.js
+++ front-end/src/components/SellerOrders/Navbar.js
@@ -3,9 +3,28 @@ import React, { Component } from 'react';
 class Navbar extends Component {
   render() {
     return (
-      <nav>
-        <h1>Navbar</h1>
-      </nav>
+      <div
+        className="nav-bar"
+      >
+        <nav>
+          {/* USAR TAG HTML APROPRIADA DEPOIS */}
+          <h1
+            className="customer_products__element-navbar-link-orders"
+          >
+            PEDIDOS
+          </h1>
+          <h1
+            className="customer_products__element-navbar-user-full-name"
+          >
+            User
+          </h1>
+          <h1
+            className="customer_products__element-navbar-link-logout"
+          >
+            Sair
+          </h1>
+        </nav>
+      </div>
     );
   }
 }
