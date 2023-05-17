ed85c98491d8121b0f8a41740bbc0257b6c9ae45
Lucas-De-Lima <lucasramosdelima@gmail.com>
2023-05-17 16:31:53 -0300

    fix: details to make login possible

--- back-end/src/database/controller/UserController.js
+++ back-end/src/database/controller/UserController.js
@@ -51,6 +51,7 @@ const create = async (req, res) => {
   const token = await generateToken({ name, email });
 
   return res.status(201).json({ token });
+<<<<<<< HEAD
 };
 
 const deleteUser = async (req, res) => {
@@ -58,6 +59,8 @@ const deleteUser = async (req, res) => {
   const allUsers = await userService.deleteId(id);
 
   return res.status(200).json(allUsers);
+=======
+>>>>>>> bf77a0a (fix: details to make login possible)
 };
 
 module.exports = {
