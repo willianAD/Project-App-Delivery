88a206effd22fbe4125f66cb50e08225a100ce88
Lucas-De-Lima <lucasramosdelima@gmail.com>
2023-05-19 18:45:36 -0300

    fix: sale details

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -23,7 +23,6 @@ function App() {
       <Route exact path="/customer/orders/:id" component={ Orders } />
       <Route exact path="/seller/orders" component={ SellerOrders } />
       <Route exact path="/seller/orders/:id" component={ SaleDetails } />
-      {/* <Route exact path="/seller/details" component={ SaleDetails } /> */}
     </Switch>
   );
 }
--- front-end/src/pages/SaleDetails.js
+++ front-end/src/pages/SaleDetails.js
@@ -1,86 +1,184 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import ProductsTable from '../components/SaleDetails/ProductsTable';
-import { requestGet, setToken } from '../services/request';
-import totalCalculate from '../utils/TotalCalulate';
-
-const dti = 'seller_order_details__element-order-details-label-delivery-status';
+import { requestGet, requestPut } from '../services/request';
+import NavbarUser from '../components/NavbarUser';
 
 class SaleDetails extends React.Component {
   constructor() {
     super();
-
     this.state = {
-      isLoading: true,
-      orderId: 0,
-      date: '',
+      sale: {},
       status: '',
-      products: [],
+      disablePreparing: true,
+      disableInTransit: true,
     };
   }
 
-  async componentDidMount() {
-    const { token } = JSON.parse(localStorage.getItem('user'));
-    setToken(token);
-    const response = await requestGet('/seller/orders/details');
-    this.setState({
-      orderId: response[0].id,
-      date: response[0].saleDate,
-      status: response[0].status,
-      products: response[0].products,
-      isLoading: false,
-    });
+  componentDidMount() {
+    this.getSale();
   }
 
+  getSale = async () => {
+    const { location: { pathname } } = this.props;
+    const saleId = pathname[pathname.length - 1];
+    const sale = await requestGet(`/seller/${saleId}`);
+    const newDate = this.formatDate(sale.saleDate);
+    this.setState({
+      sale: { ...sale, saleDate: (await newDate).toString() },
+      status: sale.status,
+      disablePreparing: sale.status !== 'Pendente',
+      disableInTransit: sale.status !== 'Preparando',
+    });
+  };
+
+  formatDate = async (date) => {
+    const dateFormat = new Date(date);
+    const ten = 10;
+    const year = dateFormat.getFullYear();
+    const day = dateFormat.getDate();
+    const wrorngFormatMonth = dateFormat.getMonth() + 1;
+    const month = wrorngFormatMonth >= ten ? wrorngFormatMonth : `0${wrorngFormatMonth}`;
+    return `${day}/${month}/${year}`;
+  };
+
+  checkPreparing = async () => {
+    const { sale } = this.state;
+    const newStatus = 'Preparando';
+    const body = {
+      userId: sale.user.id,
+      sellerId: sale.seller.id,
+      totalPrice: sale.totalPrice,
+      deliveryAddress: sale.deliveryAddress,
+      deliveryNumber: sale.deliveryNumber,
+      status: newStatus,
+    };
+    this.setState({
+      status: newStatus,
+      disableInTransit: sale.status === 'Preparando',
+      disablePreparing: true,
+    });
+    await requestPut(`/seller/${sale.id}`, body);
+  };
+
+  checkInTransit = async () => {
+    const { sale } = this.state;
+    const newStatus = 'Em Trânsito';
+    const body = {
+      userId: sale.user.id,
+      sellerId: sale.seller.id,
+      totalPrice: sale.totalPrice,
+      deliveryAddress: sale.deliveryAddress,
+      deliveryNumber: sale.deliveryNumber,
+      status: newStatus,
+    };
+    this.setState({
+      status: newStatus,
+      disableInTransit: true,
+    });
+    await requestPut(`/seller/${sale.id}`, body);
+  };
+
   render() {
-    const { isLoading, products, orderId, date, status } = this.state;
+    const dataTest = 'seller_order_details__element-order-';
+    const { sale, status, disablePreparing, disableInTransit } = this.state;
     return (
-      <>
-        <h1>Detalhe do Pedido</h1>
-        <div>
-          <h3
-            data-testid="seller_order_details__element-order-details-label-order-id"
-          >
-            {`Pedido ${orderId}`}
-          </h3>
-          <h3
-            data-testid="seller_order_details__element-order-details-label-order-date"
-          >
-            {date}
-          </h3>
-          <h3
-            data-testid={ dti }
-          >
-            {status}
-          </h3>
-          <button
-            data-testid="seller_order_details__button-preparing-check"
-            type="button"
-          >
-            Preparar pedido
-          </button>
-          <button
-            data-testid="seller_order_details__button-dispatch-check"
-            type="button"
-          >
-            Saiu para entrega
-          </button>
-        </div>
-        {
-          (isLoading) ? (<p>Carregando...</p>) : (
+      sale.products ? (
+        <>
+          <NavbarUser />
+          <h1>Detalhe do pedido</h1>
+          <div>
             <div>
-              <ProductsTable products={ products } />
+              <p
+                data-testid={ `${dataTest}details-label-order-id` }
+              >
+                PEDIDO
+                {' '}
+                { sale.id }
+              </p>
+              <p
+                data-testid={ `${dataTest}details-label-seller-name` }
+              >
+                P.Vendedora:
+                {' '}
+                { sale.seller.name }
+              </p>
+              <p
+                data-testid={ `${dataTest}details-label-order-date` }
+              >
+                { sale.saleDate }
+              </p>
+              <p
+                data-testid={ `${dataTest}details-label-delivery-status${sale.id}` }
+              >
+                { status }
+              </p>
+
+              <button
+                data-testid="seller_order_details__button-preparing-check"
+                type="button"
+                onClick={ this.checkPreparing }
+                disabled={ disablePreparing }
+              >
+                Preparar pedido
+              </button>
+              <button
+                data-testid="seller_order_details__button-dispatch-check"
+                type="button"
+                onClick={ this.checkInTransit }
+                disabled={ disableInTransit }
+              >
+                Saiu para entrega
+              </button>
             </div>
-          )
-        }
-        <h2
-          data-testid="seller_order_details__element-order-total-price"
-        >
-          {`Total: R$${totalCalculate(products).toFixed(2)}`}
-        </h2>
-      </>
+            { sale.products ? sale.products.map((a, index) => (
+              <>
+                <p
+                  data-testid={ `${dataTest}table-item-number-${index}` }
+                >
+                  { index + 1 }
+                </p>
+                <p
+                  data-testid={ `${dataTest}table-name-${index}` }
+                >
+                  { a.name }
+                </p>
+                <p
+                  data-testid={ `${dataTest}table-quantity-${index}` }
+                >
+                  { a.quantity }
+                </p>
+                <p
+                  data-testid={ `${dataTest}table-sub-total-${index}` }
+                >
+                  { a.price }
+                </p>
+                <p
+                  data-testid={ `${dataTest}table-unit-price-${index}` }
+                >
+                  {
+                    (Number(a.SalesProduct.quantity) * Number(a.price)).toFixed(2)
+                  }
+                </p>
+              </>
+            )) : <p> Loading </p> }
+            <p
+              data-testid={ `${dataTest}total-price` }
+            >
+              Total: R$
+              <spna>
+                { sale.totalPrice.replace('.', ',') }
+              </spna>
+            </p>
+          </div>
+        </>
+      ) : <p> Loading </p>
     );
   }
 }
-
+SaleDetails.propTypes = {
+  location: PropTypes.shape({
+    pathname: PropTypes.string,
+  }).isRequired,
+};
 export default connect()(SaleDetails);
